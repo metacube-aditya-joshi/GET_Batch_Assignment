2301ae29671721eb58605671f73c5389aa2d00f0
Aditya Joshi <aditya.joshi@metacube.com>
2024-11-20 12:14:51 +0530

    added some part of assginemnet 3

--- UI/UX/Assignment1/script.js
+++ UI/UX/Assignment1/script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.collapsible-header').forEach((header) => {
     header.addEventListener('click', () => {
       const body = header.nextElementSibling;
-      const isVisible = body.style.display === 'block';
-      body.style.display = isVisible ? 'none' : 'block';
+      const isVisible = body.style.display === 'flex';
+      body.style.display = isVisible ? 'none' : 'flex';
     });
   });
