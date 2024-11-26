e3efb30deca1dce0221cb38f2cbc3c38e2c04532
Aditya Joshi <aditya.joshi@metacube.com>
2024-11-26 09:38:51 +0530

    changes

---
+++ UI/UX/Assignement4/todo.ts
@@ -0,0 +1,8 @@
+/*
+? add search bar in nav bar 
+? change the title of the para below nav bar to the text selecetd
+? changin the font size of the inline html table
+? include breadcrumb
+! bootstrap affix
+
+*/
