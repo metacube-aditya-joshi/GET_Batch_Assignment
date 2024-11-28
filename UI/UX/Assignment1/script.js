4e2ec7538cd25ecf58dcf925918930a4048e56d0
Aditya Joshi <aditya.joshi@metacube.com>
2024-11-28 19:47:37 +0530

    added assignment 5

--- UI/UX/Assignment1/script.js
+++ UI/UX/Assignment1/script.js
@@ -5,3 +5,48 @@ document.querySelectorAll('.collapsible-header').forEach((header) => {
       body.style.display = isVisible ? 'none' : 'flex';
     });
   });
+
+
+
+
+  document.getElementById('fullName').addEventListener('keypress', function (event) {
+   
+    if (event.key === 'Enter') {
+      
+        event.preventDefault();
+        const name = this.value;
+        if(name.length<=2){
+          var ele=document.getElementById("fullName");
+        const custom_style={
+            color: "red"
+        }
+        Object.assign(ele.style,custom_style);
+        return;
+      }
+        
+       
+        
+        console.log(name);
+        if (name) {
+           alert(`Hi ${name}! Can I know your gender?`);
+           document.querySelector("#genderContainer").classList.remove('hidden')
+           getGender();
+        }
+    }
+});
+
+function getGender(){
+  
+  document.querySelectorAll('input[name="gender"]').forEach(radio => {
+    radio.addEventListener('change', function () {           
+             getEmail();
+    });
+})
+}
+
+function getEmail(){
+  
+  document.querySelector('#email')
+
+    alert('email fetched');
+}
